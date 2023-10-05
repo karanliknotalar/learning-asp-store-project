9c7cca2ee6292e243468a5f34c4492506ee051af
Ahmet Turan <akio@msn.com>
2023-10-05 14:47:41 +0300

    Component: ViewComponent

---
+++ StoreApp/Components/ProductSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{
+    public class ProductSummaryViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _manager;
+
+        public ProductSummaryViewComponent(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public string Invoke()
+        {
+            return _manager.ProductServices.GetAllProduct(false).Count().ToString();
+        }
+    }
+}
