98af59d42c1536f822fc992d233c5963d63d93ee
Ahmet Turan <akio@msn.com>
2023-11-04 11:39:58 +0300

    dashboard

---
+++ StoreApp/Components/CategoriesSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Components;
+
+public class CategoriesSummaryViewComponent: ViewComponent
+{
+    private readonly IServiceManager _manager;
+
+    public CategoriesSummaryViewComponent(IServiceManager manager)
+    {
+        _manager = manager;
+    }
+    
+    public string Invoke()
+    {
+        return _manager.CategoryServices.GetAllCategories().Count().ToString();
+    }
+}
